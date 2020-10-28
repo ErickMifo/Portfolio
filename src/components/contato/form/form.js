5e0aefa707fb46eac3d2651f6b50e90c7b31c219
Erick Yudi Fukuda Mifo <erickyudi99@gmail.com>
2020-10-28 03:44:36 -0300

    Formulario

---
+++ src/components/contato/form/form.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import './styles.css';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import FormikField from './FormikField/FormikField';
+import Button from '@material-ui/core/Button';
+import emailjs from 'emailjs-com';
+
+const initialValues = {
+  name: '',
+  message: '',
+  email: '',
+}
+
+
+
+const SignupSchema = Yup.object().shape({
+  name: Yup.string()
+    .required('Campo obrigatório.'),
+  message: Yup.string()
+    .required('Campo obrigatório.'),
+  email: Yup.string()
+    .email('Necessário um email váido.')
+    .required('Campo obrigatório.'),
+})
+
+const Formulario = () => {
+
+  const handleSubmit = (e , value) => {
+    e.preventDefault();
+    console.log(e.target)
+
+    emailjs.sendForm('gmail', 'template_xigp8ld', e.target , 'user_PEmMhmxPcChtmcH3hWoxO')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  }
+return(
+  <div className="formulario">
+      <Formik
+      initialValues={initialValues}
+      validationSchema={SignupSchema}
+      >
+        {({ dirty, isValid }) => {
+          return(
+            <Form onSubmit={handleSubmit}>
+              <FormikField name='name' label='Nome' />
+              <FormikField name='email' label='Email' />
+              <FormikField rows={4} multiline={true} name='message' label='Mensagem ' />
+
+              <Button variant="contained" color="primary" disabled={!dirty || !isValid} type="submit">
+                  Enviar
+              </Button>
+            </Form>
+          )
+        }}
+      </Formik>
+  </div>
+)};
+
+export default Formulario;
--- src/components/contato/index.js
+++ src/components/contato/index.js
@@ -1,12 +1,21 @@
 import { motion } from 'framer-motion';
 import React from 'react';
 import NavBar from '../navbar'
+import Formulario from './form/form';
+import './styles.css'
 
 function Contato() {
     return(
         <motion.div
         exit='out'>
             <NavBar />
+            
+            <div className="contato">
+
+            <h1> Como posso ajudar? </h1>
+            <Formulario />
+
+            </div>
         </motion.div>
     )
 }
