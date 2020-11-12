1d6d68df9e3f3263f945a663db8ffeec0b607eb3
Erick Yudi Fukuda Mifo <erickyudi99@gmail.com>
2020-11-11 21:08:08 -0300

    UPDATE: novo projeto

--- src/components/cards/cardsProjects.js
+++ src/components/cards/cardsProjects.js
@@ -3,7 +3,7 @@ import './styles.css';
 import CardItem from './cardItem';
 import chat from '../images/chat-app.png';
 import agregador from '../images/agregador.png';
-import adm from '../images/ADM.png'
+import blog from '../images/blog.png'
 
 
 
@@ -12,6 +12,12 @@ function CardsProjects() {
   return (
     <div className='cards'>
           <ul className='cards__items'>
+          <CardItem
+              src={blog}
+              text='Blog feito em gatsby. Posts criados em .md e transformados em html '
+              path='https://stoic-montalcini-357809.netlify.app/'
+              alt='Blog'
+            />
             <CardItem
               src={chat}
               text='Chat-app. projeto feito em React e Node usando socket.io;'
@@ -24,12 +30,6 @@ function CardsProjects() {
               text='Agregador de vídeos. Projeto feito durante a imersão react alura;'
               path='https://agregador-videos.vercel.app/'
               alt='Agregador de vídeos-image'
-            />
-             <CardItem
-              src={adm}
-              text='Ecommerce feito em gatsby para disciplina Administração de projetos.'
-              path='https://eager-lumiere-87e7c6.netlify.app/'
-              alt='ecommerce'
             />
           </ul>
         </div>
